cc3d1641b55fa37d1ac713c54a66a7a4e34b8cef
jejama <nyubailuwot@yahoo.co.id>
2014-06-18 20:54:50 +0700

    versi 1.0.0

---
+++ includes/configure.php
@@ -0,0 +1,31 @@
+<?php
+  define('HTTP_SERVER', 'http://elektronikkham.esy.es');
+  define('HTTPS_SERVER', 'http://elektronikkham.esy.es');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/');
+  define('HTTPS_COOKIE_PATH', '/');
+  define('DIR_WS_HTTP_CATALOG', '/');
+  define('DIR_WS_HTTPS_CATALOG', '/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+
+  define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
+  define('DIR_FS_CATALOG', '/home/u963331220/public_html/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'u963331220_rudin');
+  define('DB_SERVER_PASSWORD', 'soulm4t3');
+  define('DB_DATABASE', 'u963331220_kham');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'Asia/Jakarta');
+?>
